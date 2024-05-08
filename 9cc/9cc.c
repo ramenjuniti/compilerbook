201aee0d124bbf0349729730cda6e08d8a3acc60
kugi <ramenjuniti@gmail.com>
2024-05-08 15:47:27 +0900

    step 4

--- 9cc/9cc.c
+++ 9cc/9cc.c
@@ -20,11 +20,26 @@ struct Token {
   char *str;
 };
 
+char *user_input;
+
 Token *token;
 
 void error(char *fmt, ...) {
   va_list ap;
   va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+void error_at(char *loc, char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+
+  int pos = loc - user_input;
+  fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr, "%*s", pos, " ");
+  fprintf(stderr, "^ ");
   vfprintf(stderr, fmt, ap);
   fprintf(stderr, "\n");
   exit(1);
@@ -40,14 +55,14 @@ bool consume(char op) {
 
 void expect(char op) {
   if (token->kind != TK_RESERVED || token->str[0] != op) {
-    error("'%c'ではありません", op);
+    error_at(token->str, "'%c'ではありません", op);
   }
   token = token->next;
 }
 
 int expect_number() {
   if (token->kind != TK_NUM) {
-    error("数ではありません");
+    error_at(token->str, "数ではありません");
   }
   int val = token->val;
   token = token->next;
@@ -86,7 +101,7 @@ Token *tokenize(char *p) {
       continue;
     }
 
-    error("トークナイズできません");
+    error_at(p, "トークナイズできません");
   }
 
   new_token(TK_EOF, cur, p);
@@ -98,6 +113,8 @@ int main(int argc, char **argv) {
     error("引数の個数が正しくありません");
     return 1;
   }
+
+  user_input = argv[1];
 
   token = tokenize(argv[1]);
 
